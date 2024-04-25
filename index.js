6411b6a252388501b5e6b74e74e178343daa62f5
vgbly <31251772+vgbly@users.noreply.github.com>
2024-04-25 12:29:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-const questions = general;
-
-
+const questions;
+//const questions = general;
+//Get right JSON file
+function fetchJSONData() {
+            fetch("./questions.json")
+                .then((res) => {
+                    if (!res.ok) {
+                        throw new Error
+                            (`HTTP error! Status: ${res.status}`);
+                    }
+                    return res.json();
+                })
+                .then((data) => {
+                    questions = data.qlist;
+                    //alert(data.users[0].site);
+                    //console.log(data);
+                }
+                .catch((error) => 
+                       console.error("Unable to fetch data:", error));
+        }
+        fetchJSONData();
 
 
 let shuffledQuestions = [] //empty array to hold shuffled selected questions
